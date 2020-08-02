6446f9d903b1e2f576b1132f89524ce65639fb0c
teichmannBio <69091711+teichmannBio@users.noreply.github.com>
2020-08-02 17:38:20 +0200

    Add files via upload

---
+++ giwa_hackbio2020_.py
@@ -0,0 +1,11 @@
+#python script for Hackbio2020 task 1 by @giwa
+
+
+name = "Abdulazeez_Giwa"
+email = "giwa.ao@gmail.com"
+l_used = "python"
+biostack = "genomics_and_data_analysis"
+slack_username = "@giwa"
+
+print(name, email, l_used, biostack, slack_username)
+
