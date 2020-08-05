c64ea379d475aea2b4ce44357e72f8f7c184dc2e
teichmannBio <69091711+teichmannBio@users.noreply.github.com>
2020-08-05 21:34:33 +0200

    Delete Bernice.R

--- Bernice.R
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env Rscript
-
-Name <- "Kifordu_Bernice"
-Email <- "bernicekifordu@gmail.com"
-Language_used <- "R"
-Biostack <- "Data-Analysis"
-Slack_Username <- "@bernice"
-
-cat(Name, Email, Language_used, Biostack, Slack_Username)
