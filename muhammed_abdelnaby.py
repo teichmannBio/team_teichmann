719894c0b8e1d9ccbb2223475740a340bf53c285
teichmannBio <69091711+teichmannBio@users.noreply.github.com>
2020-08-05 17:35:41 +0100

    Add files via upload

---
+++ muhammed_abdelnaby.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+name='muhammed abdelnaby'
+email='muhammed.abdelnaby101@gmail.com'
+biostack_name='Drug Development'
+slack_username='Muhammed gabry'
+print(f"name: {name}\nemail: {email} \nbiostack name: {biostack_name}\nslack username: {slack_username}")
