ed81de81692d3c6455250076e70b12969d8c5e45
teichmannBio <69091711+teichmannBio@users.noreply.github.com>
2020-08-05 18:43:47 +0200

    Update muhammed_abdelnaby.py

--- muhammed_abdelnaby.py
+++ muhammed_abdelnaby.py
@@ -3,6 +3,6 @@
 name='muhammed_abdelnaby'
 email='muhammed.abdelnaby101@gmail.com'
 language_used = 'python'
-biostack_name='Drug Development'
+biostack_name='Drug-Development'
 slack_username='@Muhammed gabry'
 print(name, email, language_used, biostack_name, slack_username)
