acbc2697d5637c5a7751eb4206fc04624482701a
teichmannBio <69091711+teichmannBio@users.noreply.github.com>
2020-08-05 20:43:03 +0200

    Update Bernice_script.js

--- Bernice_script.js
+++ Bernice_script.js
@@ -1,2 +1,2 @@
-#!/usr/bin/env node
+#!/usr/bin/env nodejs
 document.write("Name: Kifordu Bernice<br>E-mail: bernicekifordu@gmail.com<br>Biostack: Data Analysis<br>Code Language: Javascript<br>Slack Username: @bernice")
