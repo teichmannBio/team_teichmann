657aaa07d27a252840f7a0fa2cb87048bc2c4243
teichmannBio <69091711+teichmannBio@users.noreply.github.com>
2020-08-05 21:38:08 +0200

    Delete giwa_hackbio2020_.py

--- giwa_hackbio2020_.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-name = "Abdulazeez_Giwa"
-email = "giwa.ao@gmail.com"
-l_used = "python"
-biostack = "genomics_and_data_analysis"
-slack_username = "@giwa"
-
-print(name, email, l_used, biostack, slack_username)
