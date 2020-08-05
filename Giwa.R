866e9491c55c1f287568ba15f44f5f7e8dcbaf67
teichmannBio <69091711+teichmannBio@users.noreply.github.com>
2020-08-05 20:30:52 +0100

    Add files via upload

---
+++ Giwa.R
@@ -0,0 +1,9 @@
+#!/usr/bin/env Rscript
+
+name <- "Abdulazeez_Giwa"
+email <- "giwa.ao@gmail.com"
+language_used <- "python"
+biostack <- "genomics_and_data_analysis"
+slack_username <- "@giwa"
+
+cat(name, email, language_used, biostack, slack_username)
