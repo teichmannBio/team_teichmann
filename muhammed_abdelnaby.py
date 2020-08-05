c4db09707681c0b19ec69a8d0c9ca57425150a76
teichmannBio <69091711+teichmannBio@users.noreply.github.com>
2020-08-05 18:38:38 +0200

    Update muhammed_abdelnaby.py

--- muhammed_abdelnaby.py
+++ muhammed_abdelnaby.py
@@ -4,4 +4,4 @@ name='muhammed abdelnaby'
 email='muhammed.abdelnaby101@gmail.com'
 biostack_name='Drug Development'
 slack_username='Muhammed gabry'
-print(f"name: {name}\nemail: {email} \nbiostack name: {biostack_name}\nslack username: {slack_username}")
+print(name, email, biostack_name, slack_username)
