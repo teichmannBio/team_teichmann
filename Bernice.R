97b9681f2dc741266b0fe7a792afff1dbbafd874
teichmannBio <69091711+teichmannBio@users.noreply.github.com>
2020-08-05 17:59:46 +0100

    Add files via upload

---
+++ Bernice.R
@@ -0,0 +1,9 @@
+#!/usr/bin/env Rscript
+
+Name <- "Kifordu_Bernice"
+Email <- "bernicekifordu@gmail.com"
+Language_used <- "R"
+Biostack <- "Data-Analysis"
+Slack_Username <- "@bernice"
+
+cat(Name, Email, Language_used, Biostack, Slack_Username)
