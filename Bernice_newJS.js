71381b8203028114f127f04d9e6a9d01667e6a26
teichmannBio <69091711+teichmannBio@users.noreply.github.com>
2020-08-05 22:17:43 +0200

    Delete Bernice_newJS.js

--- Bernice_newJS.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-var name = "Kifordu_Bernice";
-var email = "bernicekifordu@gmail.com";
-var language_used = "javascript";
-var biostack = "Data-Analysis";
-var slack_username = "@bernice";
-
-console.log(name, email, language_used, biostack, slack_username)
