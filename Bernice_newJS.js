b216eae89e55ce4a2fb7b774e6cf817802e9c6bc
teichmannBio <69091711+teichmannBio@users.noreply.github.com>
2020-08-05 20:22:40 +0100

    Add files via upload

---
+++ Bernice_newJS.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+var name = "Kifordu_Bernice";
+var email = "bernicekifordu@gmail.com";
+var language_used = "javascript";
+var biostack = "Data-Analysis";
+var slack_Username = "@bernice";
+
+console.log(name, email, language_used, biostack, slack_username)
