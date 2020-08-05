61a2a251f13e649326fc68b9c8ca76b18844e601
teichmannBio <69091711+teichmannBio@users.noreply.github.com>
2020-08-05 21:23:58 +0200

    Delete Bernice_script.js

--- Bernice_script.js
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env nodejs
-document.write("Name: Kifordu Bernice<br>E-mail: bernicekifordu@gmail.com<br>Biostack: Data Analysis<br>Code Language: Javascript<br>Slack Username: @bernice")
