58d9bf2919f2a7ac3c778a7876cde08774db896f
teichmannBio <69091711+teichmannBio@users.noreply.github.com>
2020-08-05 21:26:33 +0200

    Update Bernice_newJS.js

--- Bernice_newJS.js
+++ Bernice_newJS.js
@@ -4,6 +4,6 @@ var name = "Kifordu_Bernice";
 var email = "bernicekifordu@gmail.com";
 var language_used = "javascript";
 var biostack = "Data-Analysis";
-var slack_Username = "@bernice";
+var slack_username = "@bernice";
 
 console.log(name, email, language_used, biostack, slack_username)
