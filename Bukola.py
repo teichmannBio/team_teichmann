513f2ab3600d707163aced8d30e26d35d00e1dcc
teichmannBio <69091711+teichmannBio@users.noreply.github.com>
2020-08-06 16:14:55 +0100

    Add files via upload

---
+++ Bukola.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+name='Bukola Omena-Okpowe'
+email='tbfabunmi@gmail.com'
+language = 'python'
+biostack ='genomics'
+slack_username='@Bukola Omena-Okpowe'
+print(name, email, language, biostack, slack_username)
